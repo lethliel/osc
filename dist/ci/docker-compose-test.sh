fcab38cd607c038c3c19174d8a9f0f4b643e3ffb
lethliel <mstrigl@suse.com>
2019-05-28 11:23:13 +0200

    first draft of new testing

---
+++ dist/ci/docker-compose-test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+until curl http://api:3000/about 2>/dev/null ; do
+  echo "waiting for OBS to be responsive..."
+  # Print osc output incase of failure and container logs for debugging.
+  ((c++)) && ((c==60)) && (
+    curl http://api:3000/about
+    exit 1
+  )
+  sleep 1
+done
+
+cd /code
+mkdir -p ~/.config/osc/
+cp dist/ci/test.oscrc ~/.config/osc/oscrc
+python3 ./osc-wrapper.py --version
+#for file in tests/single/*tests.py; do
+#  if test -f /code/travis.settings; then
+#    COVER_ARGS="--with-coverage --cover-package=. --cover-inclusive"
+#  fi
+#  nosetests $COVER_ARGS -c .noserc -s $file 
+#done
+
+set -x
+
+if test -f /code/travis.settings; then
+  source /code/travis.settings
+  # ignore if coveralls was not setup for the repo/branch
+  run_as_tester TRAVIS_JOB_ID=$TRAVIS_JOB_ID TRAVIS_BRANCH=$TRAVIS_BRANCH TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST TRAVIS=yes coveralls || true
+fi
+
